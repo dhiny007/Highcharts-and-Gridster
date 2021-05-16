da151a3de6443a7bb27042a4744af636e378720f
Dhinesh <dhineshravi95@outlook.com>
2021-05-16 17:25:23 +0530

    highchart and gridster basic functionality done

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -1,22 +1,42 @@
-import {ChangeDetectionStrategy, Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges, OnInit, ViewEncapsulation} from '@angular/core';
 
 import {CompactType, GridsterConfig, GridsterItem, GridsterItemComponent, GridsterPush, GridType} from 'angular-gridster2';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./chart.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChartComponent implements OnInit {
   @Input() selectedVal:number;
   options: GridsterConfig;
   dashboard: GridsterItem[];
   itemToPush: GridsterItemComponent;
+  chart:Chart;
+  finalCollection;
 
   ngOnInit(): void {
+    this.selectedVal=1;
     this.options = {
       gridType: GridType.Fit,
       compactType: CompactType.None,
+      maxCols: 2,
+      minCols:1,
+      minRows: 1,
+      maxRows: 2,
+      maxItemCols: 2,
+      minItemCols: 1,
+      maxItemRows: 2,
+      minItemRows: 1,
+      maxItemArea: 2500,
+      minItemArea: 1,
+      defaultItemCols: 1,
+      defaultItemRows: 1,
+      setGridSize: true,
+      fixedColWidth: 250,
+      fixedRowHeight: 250,
       pushItems: true,
       draggable: {
         enabled: true
@@ -27,19 +47,170 @@ export class ChartComponent implements OnInit {
     };
 
     this.dashboard = [
-      {cols: 2, rows: 1, y: 0, x: 0, initCallback: this.initItem.bind(this)},
-      {cols: 2, rows: 2, y: 0, x: 2},
-      {cols: 1, rows: 1, y: 0, x: 4},
-      {cols: 3, rows: 2, y: 1, x: 4},
-      {cols: 1, rows: 1, y: 4, x: 5},
-      {cols: 1, rows: 1, y: 2, x: 1},
-      {cols: 2, rows: 2, y: 5, x: 5},
-      {cols: 2, rows: 2, y: 3, x: 2},
-      {cols: 2, rows: 1, y: 2, x: 2},
-      {cols: 1, rows: 1, y: 3, x: 4},
-      {cols: 1, rows: 1, y: 0, x: 6}
+      {cols: 1, rows: 1, y: 0, x: 0, initCallback: this.initItem.bind(this)},
+
     ];
   }
+
+  lineChart = new Chart({
+    chart: {
+      type: 'line'
+    },
+    title: {
+      text: 'Linechart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+        name: 'Line 1',
+        type:'line',
+        data: [1, 2, 3]
+      }
+    ]
+  });
+
+  barChart = new Chart({
+    chart: {
+      type: 'bar'
+    },
+    title: {
+      text: 'Bar Chart'
+    },
+    credits: {
+      enabled: false
+    },
+    series: [
+      {
+      name: 'Bar 1',
+      type:'bar',
+      data: [1, 2, 3, 4, 3.5]
+      }
+    ]
+    });
+
+  pieChart = new Chart({
+    chart:{
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: true,
+      type: 'pie'
+    },
+    title: {
+      text: 'Browser market shares in October, 2019'
+    },
+    credits:{
+      enabled:false
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+        }
+      }
+    },
+    series: [{
+      name: 'Brands',
+      colorByPoint: true,
+      type: undefined,
+      data: [{
+        name: 'Chrome',
+        y: 61.41,
+        sliced: true,
+        selected: true
+      }, {
+        name: 'Internet Explorer',
+        y: 11.84
+      }, {
+        name: 'Firefox',
+        y: 10.85
+      }, {
+        name: 'Edge',
+        y: 4.67
+      }, {
+        name: 'Safari',
+        y: 4.18
+      }]
+    }]
+  })
+
+  areaSplineChart=new Chart({
+      chart: {
+        type: 'areaspline'
+      },
+      title: {
+        text: 'Average fruit consumption during one week'
+      },
+      subtitle : {
+         style: {
+            position: 'absolute',
+            right: '0px',
+            bottom: '10px'
+      }
+      },
+      legend : {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: -150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+      },
+      xAxis:{
+         categories: ['Monday','Tuesday','Wednesday','Thursday',
+            'Friday','Saturday','Sunday']
+      },
+      yAxis : {
+         title: {
+            text: 'Number of units'
+         }
+      },
+      tooltip : {
+        shared: true, valueSuffix: ' units'
+     },
+     plotOptions : {
+        area: {
+           fillOpacity: 0.5
+        }
+     },
+     credits:{
+       enabled: false
+     },
+     series: [
+      {
+         name: 'John',
+         type:'areaspline',
+         data: [3, 4, 3, 5, 4, 10, 12]
+      },
+      {
+         name: 'Jane',
+         type:'areaspline',
+         data: [1, 3, 4, 3, 3, 5, 4]
+      }
+   ]
+  });
+
+  chartCollection=[this.lineChart,this.barChart,this.pieChart,this.areaSplineChart];
+
+  ngOnChanges(){
+    this.finalCollection=[];
+    console.log('val',this.selectedVal);
+    for(let i=0;i<this.selectedVal;i++){
+        this.finalCollection.push(this.chartCollection[i]);
+    }
+    console.log('hehehe',this.finalCollection);
+    // this.layout=this.finalCollection;
+    // console.log('layout',this.layout);
+  }
+
 
   changedOptions(): void {
     if (this.options.api && this.options.api.optionsChanged) {
